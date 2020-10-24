52f265e1f359087000fafae6aaead0e010143e39
Justinas Petrulis <justinas.petrulis@gmail.com>
2020-10-24 17:35:32 +0300

    Update spec

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"context"
 	"log"
-	
+
 	"github.com/nextprod/sdk-go/runtime"
 )
 
 func run(ctx context.Context, payload []byte) (interface{}, error) {
-	log.Info("Received request")
+	log.Println("Received request")
 	return nil, nil
 }
 
