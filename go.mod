2aa3a414eaf4827a21ebac7953efa8e1155666a7
Justinas Petrulis <justinas.petrulis@gmail.com>
2020-10-27 16:57:01 +0200

    Create event structure

--- go.mod
+++ go.mod
@@ -2,8 +2,4 @@ module github.com/nextprod/checkout
 
 go 1.13
 
-require (
-	github.com/nextprod/sdk-go v0.0.0-20201024154327-00acf98926a6
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
-	gopkg.in/src-d/go-git.v4 v4.13.1
-)
+require github.com/nextprod/sdk-go v0.0.0-20201026154513-be60ac63bae0
--- main.go
+++ main.go
@@ -7,8 +7,22 @@ import (
 	"github.com/nextprod/sdk-go/runtime"
 )
 
-func run(ctx context.Context, payload []byte) (interface{}, error) {
-	log.Printf("Event: %s", string(payload))
+// Parameters represents request parameters.
+type Parameters struct {
+	SSHKey     string `json:"ssh-key"`
+	Repository string `json:"repository"`
+	Ref        string `json:"ref"`
+	Depth      int    `json:"depth"`
+	Submodules bool   `json:"submodules"`
+}
+
+// Event represents run event.
+type Event struct {
+	Parameters Parameters `json:"parameters"`
+}
+
+func run(ctx context.Context, event Event) (interface{}, error) {
+	log.Printf("%v", event)
 	return nil, nil
 }
 
